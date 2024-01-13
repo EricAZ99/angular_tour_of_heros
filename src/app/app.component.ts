1a4a4c159f9ffab9ea24e39cce76f18a9cdecb0b
ericAZ99 <azankpoeric99@gmail.com>
2024-01-13 11:13:47 +0100

    13/01/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from "./heroes/heroes.component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeroesComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular_tour_of_heros';
+  title = 'Tour of heroes';
 }
